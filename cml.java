4e3cbc64d8503f7dd8982b553903005b721c9216
mamenjivar <menjivarmiguel221@gmail.com>
2018-04-22 16:56:43 -0700

    updating files

--- cml.java
+++ cml.java
@@ -1,44 +1,44 @@
 public class cml{
   public static void main(String[] args){
-<<<<<<< HEAD
     int count = 0;
 
     // System.out.println("Hello from world");
-    System.out.println("Hello " + args[0]);
+    // System.out.println("Hello " + args[0]);
+    System.out.println();
 
     boolean loop = true;
-
     while(loop){
 
       switch(args[count]){
         case "--help":
+          System.out.println(args[count]);
+          help();
           System.out.println("This is the help section of the program");
+          count++;
+          break;
+        case "-why":
+          System.out.println(args[count]);
+          System.out.println("this is a why section");
+          count++;
           break;
         case ".eot":
           System.out.println("This is the end of the program");
           loop = false;
-          break;
-        case "-why":
-          System.out.println("this is a why section");
+          count++;
           break;
       }
-
     }
-
-    // if(args[0].equals("--help")){
-    //   System.out.println("You will be pulling up your own github repository for people to see");
-    // }
-    // else{
-    //   System.out.println("You don't need help homie");
-    // }
-=======
-    // System.out.println("Hello from world");
-    System.out.println("Hello " + args[0]);
-    if(args[0].equals("--help")){
-      System.out.println("You will be pulling up your own github repository for people to see");
-    } else{
-      System.out.println("You don't need help homie");
-    }
->>>>>>> 10eeaffd252aa9f435b26993a1ae15ba04eff59a
   }
+
+  public void help(){
+    System.out.println("myRepositories\n");
+
+    System.out.println("\'myReposities\' returns a list of repositories based on username\n");
+
+    System.out.println("Version\t: 1.0.0");
+    System.out.println("Author\t: Miguel Menjivar");
+
+  }
+
+  public void
 }
