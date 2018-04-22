5a00d59b97c441ee01401eb7396fe623455e0c3e
mamenjivar <menjivarmiguel221@gmail.com>
2018-04-22 16:17:56 -0700

    updating files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS-380
 CS 380
+
+Computer Networks
--- cml.java
+++ cml.java
@@ -1,11 +1,34 @@
 public class cml{
   public static void main(String[] args){
+    int count = 0;
+
     // System.out.println("Hello from world");
     System.out.println("Hello " + args[0]);
-    if(args[0].equals("--help")){
-      System.out.println("You will be pulling up your own github repository for people to see");
-    } else{
-      System.out.println("You don't need help homie");
+
+    boolean loop = true;
+
+    while(loop){
+
+      switch(args[count]){
+        case "--help":
+          System.out.println("This is the help section of the program");
+          break;
+        case ".eot":
+          System.out.println("This is the end of the program");
+          loop = false;
+          break;
+        case "-why":
+          System.out.println("this is a why section");
+          break;
+      }
+
     }
+
+    // if(args[0].equals("--help")){
+    //   System.out.println("You will be pulling up your own github repository for people to see");
+    // }
+    // else{
+    //   System.out.println("You don't need help homie");
+    // }
   }
 }
